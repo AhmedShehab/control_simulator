da11c6a81e0b753ce94c324b447c59267343623a
Ahmed Shehab <ahmed4ehab@yahoo.com>
2020-12-06 14:02:21 +0200

    Added Functioning Sign in/Sign up forms

--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+# User Credentials
+class User(AbstractUser,models.Model):
+    """ status_choices=[
+        ("i","Instructor"),
+        ("s","Student")
+    ]
+    status=models.CharField(max_length=10,choices=status_choices,default="s") """
+    pass
+# Course Model
+class Course(models.Model):
+    name=models.CharField(max_length=50)
+    code=models.UUIDField(primary_key=True,default=uuid.uuid4) #Reminder: add editable=False later.
+    instructor=models.ForeignKey(User, on_delete=models.CASCADE)
+    students=models.ManyToManyField(User,related_name="students")
+
+    def __str__(self):
+        return f" Course Name: {self.name}, Course Code: {self.code}"
+    
+# Student Model 
+class Student(models.Model):
+    credentials=models.ForeignKey(User, on_delete=models.CASCADE)
+    courses= models.ManyToManyField(Course)
+    # Write all students data rows here
+    def __str__(self):
+        return f" Student Name: {self.name}"
+# Instructor Model
+class Instructor(models.Model):
+    credentials=models.ForeignKey(User, on_delete=models.CASCADE)
+    # Write all instructor data rows here
+    def __str__(self):
+        return f" Instructor Name: {self.name}"
--- main/views.py
+++ main/views.py
@@ -1,5 +1,87 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
+from .models import User,Instructor,Student,Course
 # Create your views here.
 def home(request):
-    pass
+    return render(request, "main/home.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        code=request.POST["code"]
+        agree=request.POST.get("agree")
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        # Ensure all fields are filled correctly
+        if not password or not email or not username:
+            return render(request, "main/register.html", {
+                "message": "Make sure all fields are filled."
+            })
+        # Ensure password matches confirmation
+        if password != confirmation:
+            return render(request, "main/register.html", {
+                "message": "Passwords must match."
+            })
+        if request.POST.get("student") and not code:
+            return render(request, "main/register.html", {
+                "message": "Make sure to enter your course code correctly."
+            })
+
+        # Ensure that the user agrees to all terms and privacy policy
+        if not request.POST.get("agree")=="on":
+            return render(request, "main/register.html", {
+                "message": "You need to agree on the terms in order to sign up."
+            })
+        # Attempt to create new user
+        # If the user is an instructor
+        if request.POST.get("instructor"):
+            try:
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                instructor=Instructor.objects.create(credentials=user).save()
+            except IntegrityError:
+                return render(request, "main/register.html", {
+                    "message": "Username already taken."
+                })
+        # If the user is a student
+        elif request.POST.get("student"):
+            try:
+                course=Course.objects.filter(code=code)
+                print(course)
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                student=Student.objects.create(credentials=user).save()
+            except IntegrityError:
+                return render(request, "main/register.html", {
+                    "message": "Username already taken."
+                })
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        return render(request, "main/register.html")
+
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "main/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "main/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("home"))
