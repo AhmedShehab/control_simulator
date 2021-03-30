7674f8814a7b63541b1530a3df286224650f4c53
Ahmed Shehab <ahmed4ehab@yahoo.com>
2021-03-30 05:09:15 +0200

    Merge

--- main/urls.py
+++ main/urls.py
@@ -5,14 +5,10 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("register/",views.register,name="register"),
     path("login/",views.login_view,name="login"),
-<<<<<<< HEAD
     path("student/",views.student,name="student"),
     path("instructor/",views.instructor,name="instructor"),
     path("logout/",views.logout_view,name="logout"),
     path("cruise/",views.cruise,name="cruise"),
-    path("adaptive/",views.adaptive,name="adaptive")
-=======
+    path("adaptive/",views.adaptive,name="adaptive"),
     path("test/",views.test,name="test")
->>>>>>> b8db271085c5bbef9f415147d7ede74a21fa14b0
-    
 ]
