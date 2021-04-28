7c1897b1e29d4764f7d5e8c914492653cb5ba737
Ahmed Shehab <ahmed4ehab@yahoo.com>
2021-04-28 02:08:53 +0200

    Check Point

--- main/static/main/script/register.js
+++ main/static/main/script/register.js
@@ -1,25 +1,22 @@
-function openStudentForm(){
+function openStudentForm() {
     document.getElementById("studentForm").style.display = "block";
     document.getElementById("studForm").style.display = "none";
     document.getElementById("instForm").style.display = "none";
 }
 
-/* function closeStudentForm(){
-    document.getElementById("studentForm").style.display = "none";
-    document.getElementById("studInstForm").style.display = "block";
-} */
-function openInstructorForm(){
+ function closeRegisterForm(){
+     location.reload();
+} 
+function openInstructorForm() {
     status = "i";
     document.getElementById("instructorForm").style.display = "block";
     document.getElementById("studForm").style.display = "none";
     document.getElementById("instForm").style.display = "none";
 }
-/* 
-function closeInstructorForm(){
-    document.getElementById("instructorForm").style.display = "none";
-} */
-if (document.getElementById("instMSG")){
-        openInstructorForm();
+
+
+if (document.getElementById("instMSG")) {
+    openInstructorForm();
 }
 if (document.getElementById("studMSG")) {
     openStudentForm();
