5b7e186ffc676ebe6e289b7b8b7d47c6c645cd07
GeorgeRady <george@DESKTOP-875LF52.localdomain>
2021-05-02 22:42:32 +0200

    added control functions definitions

--- main/systems.py
+++ main/systems.py
@@ -1,3 +1,27 @@
 import numpy as np
 import control
 import control.matlab as matlab
+
+def bode_Gs(sys):
+    return
+
+def bode_zpk(sys, z, p, k):
+    return
+
+def bode_pid(sys, p, i, d):
+    return
+
+def step_zpk(sys, z, p, k):
+    return
+
+def step_pid(sys, p, i, d):
+    return
+
+def stepinfo_Gs(sys):
+    return
+
+def stepinfo_zpk(sys, z, p, k):
+    return
+
+def stepinfo_pid(sys, p, i, d):
+    return
