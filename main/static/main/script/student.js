3e6ac526d34b4c55a92287844e611e8124112364
Ahmed Shehab <ahmed4ehab@yahoo.com>
2021-05-16 14:07:24 +0200

    added submission history in Student Profile

--- main/static/main/script/student.js
+++ main/static/main/script/student.js
@@ -4,3 +4,16 @@ function openReqForm(x){
 function closeReqForm(x){
     document.getElementsByClassName(x)[0].style.display="none";
 }
+function tableSelect(){
+    var status = document.getElementById("status").value;
+    var pendingTable = document.getElementById("pendingTable");
+    var submittedTable = document.getElementById("submittedTable");
+    if (status==="submitted"){
+        submittedTable.style.display="block";
+        pendingTable.style.display="none";
+    }
+    else{
+        submittedTable.style.display="none";
+        pendingTable.style.display="block";
+    }
+}
