0119cade14b6fc91a21abad90fbeaaf571d803f1
Ahmed Shehab <ahmed4ehab@yahoo.com>
2021-05-25 16:05:18 +0200

    Prettier Code

--- main/static/main/script/bode.js
+++ main/static/main/script/bode.js
@@ -1,221 +1,197 @@
 window.onload = function () {
-    var omega = document.getElementById("omega").value;
-    var mag = document.getElementById("mag").value;
-    var ph = document.getElementById("ph").value;
-    var pm = document.getElementById("pm").value;
-    var gm = document.getElementById("gm").value;
-    
-    if (parseFloat(pm)!= NaN){
-        pm = parseFloat(pm).toFixed(3);
-    }
-    var p =parseFloat(pm);
-    if(!isNaN(p)){
-        pm = parseFloat(pm).toFixed(3); 
-    }
-    var g =parseFloat(gm);
-    if(!isNaN(g)){
-        gm = parseFloat(gm).toFixed(3);  
-    }
-    
+	var omega = document.getElementById("omega").value;
+	var mag = document.getElementById("mag").value;
+	var ph = document.getElementById("ph").value;
+	var pm = document.getElementById("pm").value;
+	var gm = document.getElementById("gm").value;
 
-    omega = omega.match(/-?\d+(?:\.\d+)?/g).map(Number);
-    ph = ph.match(/-?\d+(?:\.\d+)?/g).map(Number);
-    mag = mag.match(/-?\d+(?:\.\d+)?/g).map(Number);
-    var limit = omega.length;
+	if (parseFloat(pm) != NaN) {
+		pm = parseFloat(pm).toFixed(3);
+	}
+	var p = parseFloat(pm);
+	if (!isNaN(p)) {
+		pm = parseFloat(pm).toFixed(3);
+	}
+	var g = parseFloat(gm);
+	if (!isNaN(g)) {
+		gm = parseFloat(gm).toFixed(3);
+	}
 
-    var omega_comp = document.getElementById("omega_comp");
-    var mag_comp = document.getElementById("mag_comp");
-    var ph_comp = document.getElementById("ph_comp");
+	omega = omega.match(/-?\d+(?:\.\d+)?/g).map(Number);
+	ph = ph.match(/-?\d+(?:\.\d+)?/g).map(Number);
+	mag = mag.match(/-?\d+(?:\.\d+)?/g).map(Number);
+	var limit = omega.length;
 
-    var Mag, Ph;
-    var magnitude = [];
-    var phase = [];
-    var dataSeries = { type: "line" };
-    var dataPoints = [];
-    for (var i = 0; i < limit; i += 1) {
-        
-        dataPoints.push({
-            x: omega[i],
-            y: mag[i]
-        });
-    }
-    dataSeries.dataPoints = dataPoints;
-    magnitude.push(dataSeries);
-    var dataSeries = { type: "line" };
-    var dataPoints = [];
-    for (var i = 0; i < limit; i += 1) {
-        
-        dataPoints.push({
-            x: omega[i],
-            y: ph[i]
-        });
-    }
-    dataSeries.dataPoints = dataPoints;
-    phase.push(dataSeries);
+	var omega_comp = document.getElementById("omega_comp");
+	var mag_comp = document.getElementById("mag_comp");
+	var ph_comp = document.getElementById("ph_comp");
 
+	var Mag, Ph;
+	var magnitude = [];
+	var phase = [];
+	var dataSeries = { type: "line" };
+	var dataPoints = [];
+	for (var i = 0; i < limit; i += 1) {
+		dataPoints.push({
+			x: omega[i],
+			y: mag[i],
+		});
+	}
+	dataSeries.dataPoints = dataPoints;
+	magnitude.push(dataSeries);
+	var dataSeries = { type: "line" };
+	var dataPoints = [];
+	for (var i = 0; i < limit; i += 1) {
+		dataPoints.push({
+			x: omega[i],
+			y: ph[i],
+		});
+	}
+	dataSeries.dataPoints = dataPoints;
+	phase.push(dataSeries);
 
-    Mag = {
-        zoomEnabled: true,
-        animationEnabled: true,
-        theme: "light",
-        title: {
-            display: true,
-            text: "Gain margin =" +gm +" dB/ Phase margin ="+pm +" degrees",
-            fontSize: 20
-        },
+	Mag = {
+		zoomEnabled: true,
+		animationEnabled: true,
+		theme: "light",
+		title: {
+			display: true,
+			text: "Gain margin =" + gm + " dB/ Phase margin =" + pm + " degrees",
+			fontSize: 20,
+		},
 
-        axisX:{
-            logarithmic: true,
-            title: "Frequency",
-            gridThickness: 1
-            
-        },
-        axisY: {
-            lineThickness: 1,
-            title: "Magnitude dB",
-            
-        },
-        data: magnitude
-        
-        
-    };
-    Ph = {
-        zoomEnabled: true,
-        animationEnabled: true,
-        theme: "light",
-        pointStyle:'dash',
-        title: {
-        },
-        axisX:{
-            logarithmic: true,
-            title: "Frequency",
-            gridThickness: 1
-            
-        },
-        axisY: {
-            lineThickness: 1,
-            title: "Phase",
-            
-        },
-        data: phase
-        
-    };
-    console.log(magnitude);
+		axisX: {
+			logarithmic: true,
+			title: "Frequency",
+			gridThickness: 1,
+		},
+		axisY: {
+			lineThickness: 1,
+			title: "Magnitude dB",
+		},
+		data: magnitude,
+	};
+	Ph = {
+		zoomEnabled: true,
+		animationEnabled: true,
+		theme: "light",
+		pointStyle: "dash",
+		title: {},
+		axisX: {
+			logarithmic: true,
+			title: "Frequency",
+			gridThickness: 1,
+		},
+		axisY: {
+			lineThickness: 1,
+			title: "Phase",
+		},
+		data: phase,
+	};
+	console.log(magnitude);
 
-    if ((omega_comp.value != "" || omega_comp.value==null )){
-        omega_comp = omega_comp.value;
-        mag_comp = mag_comp.value;
-        ph_comp = ph_comp.value;
-        omega_comp = omega_comp.match(/-?\d+(?:\.\d+)?/g).map(Number);
-        ph_comp = ph_comp.match(/-?\d+(?:\.\d+)?/g).map(Number);
-        mag_comp = mag_comp.match(/-?\d+(?:\.\d+)?/g).map(Number);
+	if (omega_comp.value != "" || omega_comp.value == null) {
+		omega_comp = omega_comp.value;
+		mag_comp = mag_comp.value;
+		ph_comp = ph_comp.value;
+		omega_comp = omega_comp.match(/-?\d+(?:\.\d+)?/g).map(Number);
+		ph_comp = ph_comp.match(/-?\d+(?:\.\d+)?/g).map(Number);
+		mag_comp = mag_comp.match(/-?\d+(?:\.\d+)?/g).map(Number);
 
-        var pm = document.getElementById("pm_comp").value;
-        var gm = document.getElementById("gm_comp").value;
-        
-        if (parseFloat(pm)!= NaN){
-            pm = parseFloat(pm).toFixed(3);
-        }
-        var p =parseFloat(pm);
-        if(!isNaN(p)){
-            pm = parseFloat(pm).toFixed(3); 
-        }
-        var g =parseFloat(gm);
-        if(!isNaN(g)){
-            gm = parseFloat(gm).toFixed(3);  
-        }
-        
-        var limit_comp = omega_comp.length;
-        var dataSeries = { type: "line" };
-        var dataPoints = [];
-        for (var i = 0; i < limit_comp; i += 1) {
-            
-            dataPoints.push({
-                x: omega_comp[i],
-                y: mag_comp[i]
-            });
-        }
-        dataSeries.dataPoints = dataPoints;
-        magnitude.push(dataSeries);
-        var dataSeries = { type: "line" };
-        var dataPoints = [];
-        for (var i = 0; i < limit_comp; i += 1) {
-            
-            dataPoints.push({
-                x: omega_comp[i],
-                y: ph_comp[i]
-            });
-        }
-        dataSeries.dataPoints = dataPoints;
-        phase.push(dataSeries);
-        console.log(phase)
-        Mag = {
-            zoomEnabled: true,
-            animationEnabled: true,
-            theme: "light",
-            title: {
-                display: true,
-                text: "Gain margin =" +gm +" dB/ Phase margin ="+pm +" degrees",
-                fontSize: 20
-            },
-            axisX:{
-                logarithmic: true,
-                title: "Frequency",
-                gridThickness: 1
-                
-            },
-            axisY: {
-                lineThickness: 1,
-                title: "Magnitude dB",
-                
-            },
-            axisY2:{
-                lineThickness: 1,
-                title: "Magnitude comp",
-                
-            },
-            data: magnitude
-        };
-        Ph = {
-            zoomEnabled: true,
-            animationEnabled: true,
-            theme: "light",
-            pointStyle:'dash',
-            title: {
-            },
-            axisX:{
-                logarithmic: true,
-                title: "Frequency ",
-                gridThickness: 1
-                
-            },
-            axisY: {
-                lineThickness: 1,
-                title: "Phase",
-                
-            },
-            axisY2: {
-                title: "Phase_comp",
-                lineThickness: 1
-            },
-            data: phase
-            
-        };
-        
-    }
-        var chartPP = new CanvasJS.Chart("Phase", Ph);
-        chartPP.render();
-        var chartMM = new CanvasJS.Chart("Magnitude", Mag);
-        chartMM.render();
-    
-    }
-    
-    function display(){
-        var elements = document.getElementsByClassName("no");
-        for (var i=0; i<elements.length; i++) {
-            elements[i].style.display = 'none';
-        }
-        var name= document.getElementById("selector").value;
-        document.getElementById(name).style.display ="block";
-        
-    }
+		var pm = document.getElementById("pm_comp").value;
+		var gm = document.getElementById("gm_comp").value;
+
+		if (parseFloat(pm) != NaN) {
+			pm = parseFloat(pm).toFixed(3);
+		}
+		var p = parseFloat(pm);
+		if (!isNaN(p)) {
+			pm = parseFloat(pm).toFixed(3);
+		}
+		var g = parseFloat(gm);
+		if (!isNaN(g)) {
+			gm = parseFloat(gm).toFixed(3);
+		}
+
+		var limit_comp = omega_comp.length;
+		var dataSeries = { type: "line" };
+		var dataPoints = [];
+		for (var i = 0; i < limit_comp; i += 1) {
+			dataPoints.push({
+				x: omega_comp[i],
+				y: mag_comp[i],
+			});
+		}
+		dataSeries.dataPoints = dataPoints;
+		magnitude.push(dataSeries);
+		var dataSeries = { type: "line" };
+		var dataPoints = [];
+		for (var i = 0; i < limit_comp; i += 1) {
+			dataPoints.push({
+				x: omega_comp[i],
+				y: ph_comp[i],
+			});
+		}
+		dataSeries.dataPoints = dataPoints;
+		phase.push(dataSeries);
+		console.log(phase);
+		Mag = {
+			zoomEnabled: true,
+			animationEnabled: true,
+			theme: "light",
+			title: {
+				display: true,
+				text: "Gain margin =" + gm + " dB/ Phase margin =" + pm + " degrees",
+				fontSize: 20,
+			},
+			axisX: {
+				logarithmic: true,
+				title: "Frequency",
+				gridThickness: 1,
+			},
+			axisY: {
+				lineThickness: 1,
+				title: "Magnitude dB",
+			},
+			axisY2: {
+				lineThickness: 1,
+				title: "Magnitude comp",
+			},
+			data: magnitude,
+		};
+		Ph = {
+			zoomEnabled: true,
+			animationEnabled: true,
+			theme: "light",
+			pointStyle: "dash",
+			title: {},
+			axisX: {
+				logarithmic: true,
+				title: "Frequency ",
+				gridThickness: 1,
+			},
+			axisY: {
+				lineThickness: 1,
+				title: "Phase",
+			},
+			axisY2: {
+				title: "Phase_comp",
+				lineThickness: 1,
+			},
+			data: phase,
+		};
+	}
+	var chartPP = new CanvasJS.Chart("Phase", Ph);
+	chartPP.render();
+	var chartMM = new CanvasJS.Chart("Magnitude", Mag);
+	chartMM.render();
+};
+
+function display() {
+	var elements = document.getElementsByClassName("no");
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.display = "none";
+	}
+	var name = document.getElementById("selector").value;
+	document.getElementById(name).style.display = "block";
+}
--- main/static/main/script/instructor.js
+++ main/static/main/script/instructor.js
@@ -1,73 +1,67 @@
-function openAssignmentForm(){
-    document.getElementById("assignmentForm").style.display = "flex";
-    
+function openAssignmentForm() {
+	document.getElementById("assignmentForm").style.display = "flex";
 }
-function closeAssignmentForm(){
-    document.getElementById("assignmentForm").style.display = "none";
+function closeAssignmentForm() {
+	document.getElementById("assignmentForm").style.display = "none";
 }
-function openCourseForm(){
-    document.getElementById("courseForm").style.display = "flex";
+function openCourseForm() {
+	document.getElementById("courseForm").style.display = "flex";
 }
-function closeCourseForm(){
-    document.getElementById("courseForm").style.display = "none";
+function closeCourseForm() {
+	document.getElementById("courseForm").style.display = "none";
 }
-function courseSelector(courses){
-var table = document.getElementsByClassName("tr");
-for (let i = 0; i < table.length; i++) {
-        document.getElementById(table[i].id).style.display="none "
+function courseSelector(courses) {
+	var table = document.getElementsByClassName("tr");
+	for (let i = 0; i < table.length; i++) {
+		document.getElementById(table[i].id).style.display = "none ";
+	}
+	var select = (document.getElementById("assignmentSelect").value = "Select an Assignment");
+	var course = document.getElementById("courseSelect").value;
+	var assignments = document.getElementsByClassName(course);
+	for (let i = 0; i < courses.length; i++) {
+		var assignments = document.getElementsByClassName(courses[i]);
+		if (courses[i] == course) {
+			for (let i = 0; i < assignments.length; i++) {
+				assignments[i].style.display = "block";
+			}
+		} else {
+			for (let i = 0; i < assignments.length; i++) {
+				assignments[i].style.display = "none";
+			}
+		}
+	}
 }
-var select= document.getElementById("assignmentSelect").value="Select an Assignment"
-var course = document.getElementById("courseSelect").value;
-var assignments= document.getElementsByClassName(course);
-for (let i = 0; i < courses.length; i++) {
-    var assignments= document.getElementsByClassName(courses[i]);
-    if (courses[i]==course) {
-        for (let i = 0; i < assignments.length; i++) {
-            assignments[i].style.display="block"
-        }
-    }
-    else{
-        for (let i = 0; i < assignments.length; i++) {
-            assignments[i].style.display="none"
-        }
-    }   
-    }
+function assignmentsSubmitted() {
+	var course = document.getElementById("courseSelect").value;
+	var assignments = document.getElementById("assignmentSelect").value;
+	var s = course + ":" + assignments;
+	var table = document.getElementsByClassName("tr");
+	for (let i = 0; i < table.length; i++) {
+		if (table[i].id == s) {
+			document.getElementById(table[i].id).style.display = "table-row ";
+		} else {
+			document.getElementById(table[i].id).style.display = "none";
+		}
+	}
 }
-function assignmentsSubmitted(){
-    var course = document.getElementById("courseSelect").value;
-    var assignments= document.getElementById("assignmentSelect").value;
-    var s = course+":"+assignments;
-    var table = document.getElementsByClassName("tr");
-    for (let i = 0; i < table.length; i++) {
-        if(table[i].id==s){
-            document.getElementById(table[i].id).style.display="table-row "
-        }
-        else{
-            document.getElementById(table[i].id).style.display="none"
-        }
-    }
+function gradeSelect() {
+	var grade = document.getElementById("grade").value;
+	if (grade == "auto") {
+		document.getElementById("grade2").style.display = "block";
+		document.getElementById("grade3").style.display = "none";
+		document.getElementById("rise").required = true;
+		document.getElementById("settle").required = true;
+		document.getElementById("overshoot").required = true;
+		document.getElementById("error").required = true;
+		document.getElementById("grade3").required = false;
+	}
+	if (grade == "receive") {
+		document.getElementById("grade2").style.display = "none";
+		document.getElementById("grade3").style.display = "block";
+		document.getElementById("rise").required = false;
+		document.getElementById("settle").required = false;
+		document.getElementById("overshoot").required = false;
+		document.getElementById("error").required = false;
+		document.getElementById("grade3").required = true;
+	}
 }
-function gradeSelect(){
-    var grade = document.getElementById("grade").value
-    if (grade=="auto"){
-        document.getElementById("grade2").style.display="block"
-        document.getElementById("grade3").style.display="none"
-        document.getElementById("rise").required=true
-        document.getElementById("settle").required=true
-        document.getElementById("overshoot").required=true
-        document.getElementById("error").required=true
-        document.getElementById("grade3").required=false
-
-
-    }
-    if  (grade=="receive") {
-        document.getElementById("grade2").style.display="none"
-        document.getElementById("grade3").style.display="block"
-        document.getElementById("rise").required=false
-        document.getElementById("settle").required=false
-        document.getElementById("overshoot").required=false
-        document.getElementById("error").required=false
-        document.getElementById("grade3").required=true
-    }
-}
-
